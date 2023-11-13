3a244586f618ccf101ba27abf0bc96fdf44833da
JabedAhmedRifat <jabedahmedrifat1@gmail.com>
2023-11-13 11:48:24 +0600

    admin added

--- email_marketing/admin.py
+++ email_marketing/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Sender)
+admin.site.register(Category)
+admin.site.register(Receiver)
+admin.site.register(ReceiverCategory)
+admin.site.register(Template)
+admin.site.register(History)
